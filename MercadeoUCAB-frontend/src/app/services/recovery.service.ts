515ed277472a0b34f21270745acac14fba545463
Alessandra Varisco <alessandravariscod3@gmail.com>
2020-11-17 23:09:06 -0400

    postRecovery
    Not working yet

--- MercadeoUCAB-frontend/src/app/services/recovery.service.ts
+++ MercadeoUCAB-frontend/src/app/services/recovery.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { ProcessHttpMessageService } from '../services/process-http-message.service';
+import { Observable } from 'rxjs';
+import { baseURL } from '../constants/baseURL';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class RecoveryService {
 
-  constructor() { }
+  constructor(private http: HttpClient,
+    private processHTTPMessageService: ProcessHttpMessageService) { }
+
+  postRecovery(correo): Observable<String>{
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+      })
+    };
+
+    return this.http.post<String>(baseURL + 'correo', correo, httpOptions)
+  }
 }
