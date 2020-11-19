92f7e5fc8d28e91d293e3c2a301e944841ed9c6f
Frank Hesse <hyperschnell11@outlook.sk>
2020-11-19 17:51:56 -0400

    STUDY'S PREVIEW :white_check_mark:

--- MercadeoUCAB-frontend/src/app/studies/view-study/view-study.component.ts
+++ MercadeoUCAB-frontend/src/app/studies/view-study/view-study.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { NgxSpinnerService } from "ngx-spinner";
+import { StudiesService } from '../../services/studies.service';
+import { Study } from '../../classes/study';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-view-study',
@@ -6,10 +11,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./view-study.component.scss']
 })
 export class ViewStudyComponent implements OnInit {
+  loading: boolean = true;
+  current_study: number;
+  estudio: Study;
+  studyErrorMessage: string;
 
-  constructor() { }
+  constructor(private Activatedroute: ActivatedRoute,
+    private router: Router,
+    private location: Location,
+    private studiesService: StudiesService,
+    private spinner: NgxSpinnerService) {
+    /* If query is empty return 404 */
+    if ((this.Activatedroute.snapshot.queryParamMap.get('sid') || 0) == 0) {
+      this.router.navigate(['404']);
+    }
+
+    /* Get current study */
+    else {
+      this.spinner.show();
+
+      this.current_study = parseInt(this.Activatedroute.snapshot.queryParamMap.get('sid'));
+      this.studiesService.getStudy(this.current_study).subscribe((study) => {
+        
+        if (!study) {
+          this.router.navigate(['404']);
+        }
+
+        this.estudio = study;
+
+        this.spinner.hide();
+        this.loading = false;
+      }, errorMessage => {
+        this.loading = false;
+        this.spinner.hide();
+        this.studyErrorMessage = errorMessage;
+      });
+    }
+  }
 
   ngOnInit(): void {
+  }
+
+  backClicked() {
+    this.location.back();
   }
 
 }
