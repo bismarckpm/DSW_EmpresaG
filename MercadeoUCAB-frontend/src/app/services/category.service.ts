f352906436e2638e93baade045164b3a06d8b927
AngelMCC99 <57779761+AngelMCC99@users.noreply.github.com>
2020-11-26 00:38:03 -0430

    Categorias y añadir categoria

--- MercadeoUCAB-frontend/src/app/services/category.service.ts
+++ MercadeoUCAB-frontend/src/app/services/category.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { baseURL } from '../constants/baseURL';
 import { catchError } from 'rxjs/operators';
 import { ProcessHttpMessageService } from '../services/process-http-message.service';
@@ -16,6 +16,33 @@ export class CategoryService {
 
   getCategories(): Observable<Category[]> {
     return this.http.get<Category[]>(baseURL + 'categories')
+      .pipe(catchError(this.processHTTPMessageService.handleError))
+  }
+
+  postCategory(category): Observable<Category>{
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+      })
+    };
+
+    return this.http.post<Category>(baseURL + 'categories', category, httpOptions)
+      .pipe(catchError(this.processHTTPMessageService.handleError))
+  }
+
+  putPresentation(category): Observable<Category>{
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+      })
+    };
+
+    return this.http.put<Category>(baseURL + 'categories/' + category.id, category, httpOptions)
+      .pipe(catchError(this.processHTTPMessageService.handleError))
+  }
+
+  deleteCategory(category): Observable<Category>{
+    return this.http.delete<Category>(baseURL + 'categories/' + category.id)
       .pipe(catchError(this.processHTTPMessageService.handleError))
   }
 }
