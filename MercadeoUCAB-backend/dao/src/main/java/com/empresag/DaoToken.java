ef3dd092c9abe6f42360552850611eac3f2936c1
AngelMCC99 <57779761+AngelMCC99@users.noreply.github.com>
2020-12-15 23:59:22 -0430

    recovery password 65%

--- MercadeoUCAB-backend/dao/src/main/java/com/empresag/DaoToken.java
+++ MercadeoUCAB-backend/dao/src/main/java/com/empresag/DaoToken.java
@@ -1,11 +1,58 @@
 package com.empresag;
 
 import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+import java.util.List;
+import java.util.Random;
 
 public class DaoToken extends Dao<TokenEntity> {
     private EntityManager _em;
     static DaoHandler _handler = new DaoHandler();
+
+    String JPQL = null;
+    Query q = null;
+
     public DaoToken(){
         super(_handler);
+    }
+
+    public String getHASH(){
+        char[] chars = "abcdefghijklmnopqrstuvwxyz123456789".toCharArray();
+        StringBuilder sb = new StringBuilder(10);
+        Random random = new Random();
+        for (int i = 0; i < 25; i++) {
+            char c = chars[random.nextInt(chars.length)];
+            sb.append(c);
+        }
+        String hash = sb.toString();
+        return hash;
+    }
+
+    public TokenEntity findToken(long fkUsuario){
+        TokenEntity token = new TokenEntity();
+        DaoUsuario usuarioDao = new DaoUsuario();
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("empresag");
+        EntityManager em = emf.createEntityManager();
+
+        UsuarioEntity user = usuarioDao.find(fkUsuario,UsuarioEntity.class);
+
+        JPQL = "SELECT t FROM TokenEntity t WHERE t.fkUsuario._id = :fkUsuario";
+        q = em.createQuery(JPQL);
+        q.setParameter("fkUsuario", fkUsuario);
+
+        List<TokenEntity> tokens = q.getResultList();
+
+        if (!tokens.isEmpty())
+            token = tokens.get(0);
+
+        return token;
+    }
+
+    public void deleteTokenReset(long id_usuario){
+        TokenEntity token = findToken(id_usuario);
+        token.setToken_reset(null);
+        update(token);
     }
 }
--- MercadeoUCAB-backend/servicio/src/test/java/LoginTest.java
+++ MercadeoUCAB-backend/servicio/src/test/java/LoginTest.java
@@ -1,23 +1,45 @@
-import com.empresag.UsuarioEntity;
-import com.empresag.DaoUsuario;
-import com.empresag.LoginService;
+import com.empresag.*;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.List;
+
 public class LoginTest {
+//    @Test
+//    public void getUsuario(){
+//        LoginService login = new LoginService();
+//        DaoUsuario usuarioDao = new DaoUsuario();
+//
+//
+//        String userString = "";
+//        UsuarioEntity user = login.currentUser("angel99castillo@gmail.com", "Am101199.");
+//        if (user != null)
+//            userString = user.toString();
+//
+//        UsuarioEntity userPrueba = usuarioDao.find((long)1,UsuarioEntity.class);
+//
+//        Assert.assertEquals( userPrueba.toString(), userString );
+//    }
+
+//    @Test
+//    public void recClaveTest(){
+//        UserService us = new UserService();
+//        RecoveryService recovery = new RecoveryService();
+//        DaoToken tokenDao = new DaoToken();
+//
+//        //UsuarioDto dtoUsuario = us.getUser(1);
+//
+//        String resp = recovery.correoRecuperar("angel99castillo@gmail.com");
+//
+//        //TokenEntity token = tokenDao.findToken(1);
+//
+//        Assert.assertEquals("success",resp);
+//        //Assert.assertEquals(1,token.get_id());
+//    }
+
     @Test
-    public void getUsuario(){
-        LoginService login = new LoginService();
-        DaoUsuario usuarioDao = new DaoUsuario();
-
-
-        String userString = "";
-        UsuarioEntity user = login.currentUser("angel99castillo@gmail.com", "Am101199.");
-        if (user != null)
-            userString = user.toString();
-
-        UsuarioEntity userPrueba = usuarioDao.find((long)1,UsuarioEntity.class);
-
-        Assert.assertEquals( userPrueba.toString(), userString );
+    public void changeTest(){
+        RecoveryService rec = new RecoveryService();
+        rec.changePassword("angel99castillo@gmail.com","Caracas1.");
     }
 }
